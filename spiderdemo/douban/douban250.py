8e4aff8b9508ed4d8f5ab56ddaf9f4b816f6e2fa
kingstar718 <kingstar718@foxmail.com>
2019-06-09 22:46:00 +0800

    book信息存入数据库

--- spiderdemo/douban/douban250.py
+++ spiderdemo/douban/douban250.py
@@ -3,6 +3,7 @@ import requests
 from urllib.request import urlopen
 import re
 import json
+import pymysql
 
 
 def get_bs(url):
@@ -36,7 +37,7 @@ def write_to_file(content):
         print(type(json.dumps(content)))
         f.write(json.dumps(content, ensure_ascii=False)+'\n')
 
-
+# 存入文本文件
 def main(start):
     url = 'https://book.douban.com/top250?start=' + str(start)
     bsObj = get_bs(url)
@@ -47,6 +48,69 @@ def main(start):
         write_to_file(item)
 
 
+# 存入数据库
+def main2(start, cursor, db):
+    url = 'https://book.douban.com/top250?start=' + str(start)
+    bsObj = get_bs(url)
+    # print(get_content(bsObj))
+    for i, data in enumerate(get_content(bsObj)):
+        print(i, data)
+        data['bookId'] = i+start
+        table = 'doubanbook250'
+        keys = ','.join(data.keys())  # id,name,age
+        values = ','.join(['%s'] * len(data))  # %s,%s,%s
+        sql = "INSERT INTO {table}({keys}) values ({values})".format(table=table, keys=keys, values=values)
+        cursor.execute(sql, tuple(data.values()))
+        db.commit()
+        '''
+        try:
+            if cursor.execute(sql, tuple(data.values())):  # 第二个参数传入data的键值构造的元组，
+                print("Successful")
+                db.commit()
+        except:
+            print("Failed")
+            db.rollback()
+        '''
+    '''
+    data = {
+        'bookId': 1,
+        'bookName': '哈利·波特与阿兹卡班的囚徒',
+        'bookAddress': 'https://book.douban.com/subject/1071241/',
+        'bookImg': 'https://img3.doubanio.com/view/subject/m/public/s1074376.jpg',
+        'bookAuthor': '[英] J. K. 罗琳 / 郑须弥 / 人民文学出版社 / 2000-9 / 26.50元',
+        'bookScore': '9.0',
+        'bookEvaluate': '102489'
+    }
+    table = 'doubanbook250'
+    keys = ','.join(data.keys())  # id,name,age
+    values = ','.join(['%s'] * len(data))  # %s,%s,%s
+    print(keys)
+    print(values)
+    sql = "INSERT INTO {table}({keys}) values ({values})".format(table=table, keys=keys, values=values)
+    print(sql)
+    try:
+        if cursor.execute(sql, tuple(data.values())):   # 第二个参数传入data的键值构造的元组，
+            print("Successful")
+            db.commit()
+    except:
+        print("Failed")
+        db.rollback()
+    db.close()
+    '''
+
+
 if __name__ == "__main__":
+    '''
     for i in range(10):
         main(i*25)
+    '''
+    '''
+    # 将数据存入数据库
+    db1 = pymysql.connect(host='localhost', user='root', password='wujinxing718', port=3306, database='spiders')
+    cursor1 = db1.cursor()
+    for i in range(10):
+        main2(i*25, cursor1, db1)
+    db1.commit()
+    db1.close()
+    '''
+    pass
